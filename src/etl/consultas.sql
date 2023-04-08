c7fd00b41489301e8e7fb11cc3ac28ae7de1f640
Emy Kuroiwa <emy.kuroiwa@indicium.tech>
2023-04-08 12:54:46 -0300

    Criando o arquivo pagamentos.sql

---
+++ src/etl/consultas.sql
@@ -0,0 +1,13 @@
+-- SELECT *
+-- FROM pagamento_pedido
+
+-- select 
+--     date(dtPedido) as order_date
+--     , count(*) as order_qty
+-- from pedido
+-- group by order_date
+
+
+select *
+from item_pedido
+
---
+++ src/etl/pagamentos.sql
@@ -0,0 +1,13 @@
+
+with payment_type as (
+
+    select
+    from pedido as pd
+    left join
+
+    select *
+    from pedido
+    where dtPedido < '2018-01-01'
+        and dtPedido >= date('2018-01-01', '-6 month')
+
+)
